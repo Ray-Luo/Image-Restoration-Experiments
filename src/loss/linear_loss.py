34f71133d052ce225350f429e58aeeaa7e6009d9
leiluoray <leiluo.ray@gmail.com>
2023-03-28 17:54:50 -0700

    more

--- src/loss/linear_loss.py
+++ src/loss/linear_loss.py
@@ -5,8 +5,5 @@ class LinearLoss:
     def __init__(self):
         self.loss = L1Loss()
 
-    def denormalize(self, x):
-        return x * 4000.0
-
     def __call__(self, pred, gt):
-        return self.loss(self.denormalize(pred), self.denormalize(gt))
+        return self.loss(pred, gt) * 4000.0
