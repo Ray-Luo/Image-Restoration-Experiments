ecce0fd38a0749b2612697cc3b0b1d0300c4b9d6
leiluoray <leiluo.ray@gmail.com>
2023-04-21 14:20:38 -0700

    Update test_lib.py

--- test_lib.py
+++ test_lib.py
@@ -1,36 +1,13 @@
-# example.py
+import subprocess
 
-import os
-import ctypes
-import ctypes.util
-
-libs_path = "/home/luoleyouluole/Image-Restoration-Experiments/shared_libs/"
-
-for file_name in os.listdir(libs_path):
-    print(file_name,"*****")
-    ctypes.util.find_library(os.path.join(libs_path, file_name))
-
-lib = ctypes.cdll.LoadLibrary("libxplat_compphoto_gpuEngine_mattingToolCpyFbcode.so")
-lib.applyMatting.argtypes = [
-    ctypes.c_char_p, # string maskPath,
-    ctypes.c_char_p, # string guidancePath,
-    ctypes.c_char_p, # string outputPath,
-    ctypes.c_float, # float e,
-    ctypes.c_float, # float s,
-    ctypes.c_int, # int r,
-    ctypes.c_bool, # bool gRgb,
-    ctypes.c_bool # bool iRgb,
-]
-lib.applyMatting.restype = ctypes.c_void_p
-
-
-maskPath = "/data/users/luoleyouluole/fbsource/xplat/compphoto/gpuEngine/test/testData/matting/person1_mask.png"
-guidancePath = "/data/users/luoleyouluole/fbsource/xplat/compphoto/gpuEngine/test/testData/matting/person1.png"
-outputPath = "./python_tres.png"
+maskPath = "/home/luoleyouluole/fbsource/xplat/compphoto/gpuEngine/test/testData/matting/person1_mask.png"
+guidancePath = "/home/luoleyouluole/fbsource/xplat/compphoto/gpuEngine/test/testData/matting/person1.png"
+outputPath = "/home/luoleyouluole/Image-Restoration-Experiments/python_res.png"
 r = 10
 s = 0.25
 e = 1e-4
 gRgb = True
 iRgb = False
 
-lib.applyMatting(maskPath.encode(), guidancePath.encode(), outputPath.encode(), e, s, r, gRgb, iRgb)
+# Call the C++ binary with arguments
+subprocess.run(["/home/luoleyouluole/fbsource/buck-out/gen/aab7ed39/xplat/compphoto/gpuEngine/mattingToolFbcode", "--mask", maskPath, "--guidance", guidancePath, "--output", outputPath, "--epsilon", str(e), "--scaleF", str(s), "--radius", str(r), "--guidanceRGB", str(gRgb), "--inputRGB", str(iRgb)], capture_output=True)
