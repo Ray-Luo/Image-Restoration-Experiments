25bdf611ecbdb322c061ccc2ee630454427968e2
leiluoray <leiluo.ray@gmail.com>
2023-04-21 15:26:27 -0700

    more

--- src/loss/pu_forward_loss.py
+++ src/loss/pu_forward_loss.py
@@ -10,7 +10,8 @@ class PUForwardLoss:
         self.loss = L1Loss()
 
     def pu(self, x):
-        # x *= 4000.0
+        x = torch.clamp(x, min=1e-6)
+        x = x * 4000.0
         return torch.log2(A_COEFF * x + B_COEFF)
 
     def __call__(self, pred, gt):
