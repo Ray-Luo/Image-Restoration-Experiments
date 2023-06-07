8bb40d809e172ff63e860dbb9daebbff92c839df
leiluoray <leiluo.ray@gmail.com>
2023-06-07 16:34:46 -0700

    more

--- src/get_file_names.py
+++ src/get_file_names.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 # Specify the directory you want to scan
-folder_path = '/home/luoleyouluole/Image-Restoration-Experiments/data/HDR_VIDEO_FRAME_4xd_patchify'
+folder_path = '/home/luoleyouluole/Image-Restoration-Experiments/data/240p_hdr_super_fblearner/240p_valid_4xd_patchify'
 
 # Get a list of all files in the directory
 file_list = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
@@ -11,4 +11,4 @@ file_list = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(f
 df = pd.DataFrame(file_list, columns=['filename'])
 
 # Write the DataFrame to a CSV file
-df.to_csv('HDR_VIDEO_FRAME_patch_lq.csv', index=False)
+df.to_csv('240p_valid_4xd_patchify.csv', index=False)
