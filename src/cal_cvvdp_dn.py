ad4572d51ce935bb227ec14f76fb0f9db5b90050
leiluoray <leiluo.ray@gmail.com>
2023-07-04 10:16:15 -0700

    more

--- src/cal_cvvdp_dn.py
+++ src/cal_cvvdp_dn.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 # directory containing the images
-test_img_folder = '/home/luoleyouluole/Image-Restoration-Experiments/data/res_dn'
+test_img_folder = '/home/luoleyouluole/Image-Restoration-Experiments/data/res_sad'
 imgs = os.listdir(test_img_folder)
 imgs.sort()
 test_imgs = []
