bbd49fa61df9c90a37352b610352ab0a275c3079
leiluoray <leiluo.ray@gmail.com>
2023-07-25 11:02:20 -0700

    more

--- src/cal_cvvdp_real.py
+++ src/cal_cvvdp_real.py
@@ -184,7 +184,7 @@ report += "linear_pq_cvvdp = " + str(linear_pq_cvvdp) + "\n"
 report += "linear_mu_cvvdp = " + str(linear_mu_cvvdp) + "\n"
 
 
-with open("/home/luoleyouluole/Image-Restoration-Experiments/src/real_report.txt", "w") as file:
+with open("/home/luoleyouluole/Image-Restoration-Experiments/src/real_report_extra.txt", "w") as file:
     file.write(report)
 
 
